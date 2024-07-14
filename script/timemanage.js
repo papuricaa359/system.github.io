194b15645fb01aac9bb8ee0c8310243aedf021cb
papuricaa359 <papuricaa359@gmail.com>
2024-07-14 19:01:37 +0900

    Add files via upload

--- script/timemanage.js
+++ script/timemanage.js
@@ -3,7 +3,7 @@
       e.preventDefault();
       var remainingTime = document.getElementById('remainingTime').value;
       
-      fetch(`https://script.google.com/macros/s/AKfycbyv8poRTwrFioc0bCi4sg9CntrRUgSvoNRmJHOnIMtKgrSCQ6Gc5PSThhNWl9Om6Ki6/exec?remainingTime=${remainingTime}`)
+      fetch(`https://script.google.com/macros/s/AKfycbzgr4JpmFFwn5lRfwmlUY5b6JcHr04FMuY3iT1KbrPjIiiUXH4d4OvGN7mR8qHmleew/exec?remainingTime=${remainingTime}`)
         .then(response => response.text())
         .then(data => {
           document.getElementById('remainingTime').value = '';
--- script/timeshow.js
+++ script/timeshow.js
@@ -1,9 +1,10 @@
 
     function updateRemainingTime() {
-      fetch('https://script.google.com/macros/s/AKfycbx6fV6m2DavFK46mwVOa6Wm5uDDjD9ViNx7E6q9bS38mgEK7WmgkB52bYetJ0xIwZ7_/exec?service=latestTime')
+      fetch('https://script.google.com/macros/s/AKfycbzgr4JpmFFwn5lRfwmlUY5b6JcHr04FMuY3iT1KbrPjIiiUXH4d4OvGN7mR8qHmleew/exec?service=latestTime')
         .then(response => response.text())
         .then(time => {
           document.getElementById('remainingTime').innerText = time;
+          document.getElementById('waitTimesmart').innerText = time;
         });
     }
 
