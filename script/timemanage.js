a05ef45d1721bded198733efcf7d97c005784e1c
papuricaa359 <papuricaa359@gmail.com>
2024-07-16 09:33:31 +0900

    Update timemanage.js

--- script/timemanage.js
+++ script/timemanage.js
@@ -2,13 +2,14 @@
     document.getElementById('timeForm').addEventListener('submit', function(e) {
       e.preventDefault();
       var remainingTime = document.getElementById('remainingTime').value;
-      if(remainingTime>0){
+      if(remainingTime>=0){
       fetch(`https://script.google.com/macros/s/AKfycbzgr4JpmFFwn5lRfwmlUY5b6JcHr04FMuY3iT1KbrPjIiiUXH4d4OvGN7mR8qHmleew/exec?remainingTime=${remainingTime}`)
         .then(response => response.text())
         .then(data => {
           document.getElementById('remainingTime').value = '';
         });
       }else{
-          
+          alert("有効な待ち時間を入力してください。");
+                return;
       }
     });
