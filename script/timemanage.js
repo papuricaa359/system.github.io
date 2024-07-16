3c59c0a9726e811668363472ae7726afefb25bf5
papuricaa359 <papuricaa359@gmail.com>
2024-07-16 09:31:33 +0900

    Update timemanage.js

--- script/timemanage.js
+++ script/timemanage.js
@@ -2,10 +2,13 @@
     document.getElementById('timeForm').addEventListener('submit', function(e) {
       e.preventDefault();
       var remainingTime = document.getElementById('remainingTime').value;
-      
+      if(remainingTime>0){
       fetch(`https://script.google.com/macros/s/AKfycbzgr4JpmFFwn5lRfwmlUY5b6JcHr04FMuY3iT1KbrPjIiiUXH4d4OvGN7mR8qHmleew/exec?remainingTime=${remainingTime}`)
         .then(response => response.text())
         .then(data => {
           document.getElementById('remainingTime').value = '';
         });
+      }else{
+          
+      }
     });
