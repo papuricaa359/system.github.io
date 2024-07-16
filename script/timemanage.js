5a4883981f2b2cb3b47d56c2753d90b616777e4e
papuricaa359 <papuricaa359@gmail.com>
2024-07-16 09:36:27 +0900

    Update timemanage.js

--- script/timemanage.js
+++ script/timemanage.js
@@ -1,15 +1,17 @@
 
     document.getElementById('timeForm').addEventListener('submit', function(e) {
-      e.preventDefault();
-      var remainingTime = document.getElementById('remainingTime').value;
-      if(remainingTime>=0){
-      fetch(`https://script.google.com/macros/s/AKfycbzgr4JpmFFwn5lRfwmlUY5b6JcHr04FMuY3iT1KbrPjIiiUXH4d4OvGN7mR8qHmleew/exec?remainingTime=${remainingTime}`)
-        .then(response => response.text())
-        .then(data => {
-          document.getElementById('remainingTime').value = '';
-        });
-      }else{
-          alert("有効な待ち時間を入力してください。");
-                return;
-      }
-    });
+  e.preventDefault();
+  var remainingTime = document.getElementById('remainingTime').value;
+  if (remainingTime >= 0) {
+    fetch(`https://script.google.com/macros/s/AKfycbzgr4JpmFFwn5lRfwmlUY5b6JcHr04FMuY3iT1KbrPjIiiUXH4d4OvGN7mR8qHmleew/exec?remainingTime=${remainingTime}`)
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('remainingTime').value = '';
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  } else {
+    alert("有効な待ち時間を入力してください。");
+  }
+});
